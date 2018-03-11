3d15232adfd1b4db8c4b00391f78331257583d0f
Afforess <afforess@gmail.com>
2018-03-11 13:33:33 -0500

    Add record.sh script

---
+++ record.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# https://stackoverflow.com/a/252911/1634249
+WINDOW=$(xwininfo -tree -root | grep -i "psensor - temperature monitor")
+if [ $? -ne 0 ]; then
+    echo "no window matching 'pSensor - Temperature Monitor'"
+    exit 2
+fi
+# ifs string splitting on whitespace
+WINDOW_ARGS=( $WINDOW )
+WINDOW_ID=${WINDOW_ARGS[0]}
+
+while true; do
+    import -window $WINDOW_ID sensors.png
+    sleep 1
+done
