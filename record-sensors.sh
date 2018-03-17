f0c327cda7fbbafaa714d31430879578f69fe34b
Afforess <afforess@gmail.com>
2018-03-17 09:17:02 -0500

    Harden window id lookup and check on each loop

--- record-sensors.sh
+++ record-sensors.sh
@@ -7,17 +7,25 @@ if [ -z "$1" ]; then
     export DISPLAY=:0
     export XAUTHORITY=/run/user/1000/gdm/Xauthority
 fi
-# https://stackoverflow.com/a/252911/1634249
-WINDOW=$(xwininfo -tree -root | grep -i "psensor - temperature monitor")
-if [ $? -ne 0 ]; then
-    echo "no window matching 'pSensor - Temperature Monitor'"
-    exit 2
-fi
-# ifs string splitting on whitespace
-WINDOW_ARGS=( $WINDOW )
-WINDOW_ID=${WINDOW_ARGS[0]}
+
+function psensor_window_id() {
+    # https://stackoverflow.com/a/252911/1634249
+    local WINDOW=$(xwininfo -tree -root | grep -i "psensor - temperature monitor")
+    if [ $? -ne 0 ]; then
+        return 1
+    fi
+    # ifs string splitting on whitespace
+    local WINDOW_ARGS=( $WINDOW )
+    echo ${WINDOW_ARGS[0]}
+}
 
 while true; do
+    WINDOW_ID=$( psensor_window_id )
+    if [[ -z $WINDOW_ID ]]; then
+        echo "no window found matching 'pSensor - Temperature Monitor', waiting for window..."
+        sleep 2
+        continue
+    fi
     echo "import -window $WINDOW_ID sensors.png"
     import -window $WINDOW_ID "${CWD}/sensors.png"
     sleep 2
